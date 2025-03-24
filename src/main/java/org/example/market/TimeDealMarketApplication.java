6fc09fdd188ac2f5f404a2cfbb642b69c0a0d2e5
hanna <hannah.k0925@gmail.com>
2025-03-24 17:14:53 +0900

    chore(init)

---
+++ src/main/java/org/example/market/TimeDealMarketApplication.java
@@ -0,0 +1,13 @@
+package org.example.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeDealMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeDealMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/market/TimeDealMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeDealMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
